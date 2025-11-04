3375cc3955335a68d5693fd9b6d714f68dd69b27
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-11-04 14:11:08 -0300

    Aprendendo sobre isinstance em python

--- aula01/variaveis02.py
+++ aula01/variaveis02.py
@@ -1,9 +1,18 @@
 media = 0
 n1 = n2 = n3 = n4 = 0.0
-
 nome, idade = 'Fabio', 47
+estado = True
 
 #Função type() informa o tipo da variável
-print(type(media))
-print(type(n2))
-print(type(nome))
+print(type(media)) #inteiro
+print(type(n2)) #float
+print(type(nome)) #string
+print(type(estado)) #booleano
+print(type(1+2j)) #número complexo
+
+# Função isinstance()
+
+a = 10
+b = 'Sol'
+print(isinstance(a,(int, float))) #True
+print(isinstance(b, str)) #True 
