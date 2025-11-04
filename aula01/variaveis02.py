1d5f378fe771c796472caf63502bc9d82a9f2f1f
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-11-04 15:30:20 -0300

    Operadores aritmeticos em python

--- aula01/variaveis02.py
+++ aula01/variaveis02.py
@@ -4,15 +4,20 @@ nome, idade = 'Fabio', 47
 estado = True
 
 #Função type() informa o tipo da variável
-print(type(media)) #inteiro
-print(type(n2)) #float
-print(type(nome)) #string
-print(type(estado)) #booleano
-print(type(1+2j)) #número complexo
+# print(type(media)) #inteiro
+# print(type(n2)) #float
+# print(type(nome)) #string
+# print(type(estado)) #booleano
+# print(type(1+2j)) #número complexo
 
 # Função isinstance()
 
-a = 10
-b = 'Sol'
-print(isinstance(a,(int, float))) #True
-print(isinstance(b, str)) #True 
+# a = 10
+# b = 'Sol'
+# print(isinstance(a,(int, float))) #True
+# print(isinstance(b, str)) #True 
+
+a = 40
+c = 3
+r = a * c
+print(r) 
