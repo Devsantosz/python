521ab401241d7e33e6cac2661186b47b0301205c
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-11-24 17:55:10 -0300

    Aprendendo sobre listas em PYTHON

---
+++ aula11/listas.py
@@ -0,0 +1,30 @@
+#lista: Representa sequencia de valores
+
+#Sintaxe: nome_lista = []
+
+# notas = [5,7,8,6,9]
+# print(notas)
+
+n1 = [4,6,7,8,0,3]
+n2 = [1,6,3,0,12,11]
+valores = n1 + n2
+# valores[0] = 9
+# print(len(valores))
+# print(sorted(valores, reverse=True))
+# print(sum(valores))
+# print(min(valores))
+# print(max(valores))
+
+# valores.append(13)
+# print(valores)
+# valores.pop()
+# print(valores)
+# valores.pop(3)
+# print(valores)
+# valores.insert(3,21)
+# print(valores)
+# print(17 in valores)
+
+planetas = ['Mercurio','Venus','Marte','Saturno','Urano','Netuno']
+for planeta in planetas:
+    print(planeta)
