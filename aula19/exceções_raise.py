0af789fa36c80bb51ef6cea09e03bf9a101a8b88
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-12-05 18:04:33 -0300

    Aprendendo sobre exceções/raise em PYTHON

---
+++ aula19/exceções_raise.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+class NumeroNegativoError(Exception):
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    try:
+        num = int(input('Digite um numero positivo:'))
+        if num < 0:
+            raise NumeroNegativoError
+    except NumeroNegativoError:
+        print(f'Foi fornecido um numero negativo!')
+    else:
+        print(f'A raiz quadrad de {num} é {sqrt(num)}')
+    finally:
+        print(f'Fim do calculo.')
