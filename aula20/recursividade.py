e6b1797ecc1f7f5f821358489fa074817bb4bc30
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-12-11 17:25:04 -0300

    Aprendendo sobre Funções recursivas em PYTHON

---
+++ aula20/recursividade.py
@@ -0,0 +1,22 @@
+#Recursividade em Python
+
+#Formula geral para o fatorial
+#Fatorial(num) = 1, se num = 0 ou se num = 1 'Caso-Base'
+#Fatorial(num) = num * Fatorial(num - 1), para num > 1 'caso Recursivo'
+# 4! = 4 * fatorial(3) = 4 * 3 * fatorial(2) = 4 * 3 * 2 * fatorial(1) 
+# 4 * 3 * 2 * 1 = 24
+
+def fatorial(numero):
+    if numero == 0 or numero == 1: #caso-base
+        return 1
+    else: #caso-recursivo
+        return numero * fatorial(numero - 1)
+
+if __name__ == '__main__':
+    x = int(input('Digite um numero inteiro para calcular seu fatorial: '))
+    try:
+        res = fatorial(x)
+    except RecursionError:
+        print('Numero muito grande para calcular o fatorial')
+    else:
+        print(f'O fatorial de {x} é igual a {res}')
